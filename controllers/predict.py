56af0db4cb432bb1ed55c3d69391e6a14057eeb9
parthnamdev <parthnamdev@users.noreply.github.com>
2022-12-23 11:52:27 +0530

    test: xgboost model

--- controllers/predict.py
+++ controllers/predict.py
@@ -3,9 +3,13 @@
 import sys
 import tensorflow as tf
 import statistics
+import pickle
 # with open(r"model.pkl", "rb") as input_file:
 #   m = pickle.load(input_file)
-m = tf.keras.models.load_model('./model.pb')
+# m = tf.keras.models.load_model('./model.pb')
+import xgboost as xgb
+m = xgb.XGBRegressor()
+m.load_model('../xmodel.json')
 
 # from load import m
 l=[]
@@ -18,7 +22,10 @@ for i in range(1,len(sys.argv)):
   l.append(res)
 
 # print(l)
-res = m.predict(l)
+sc = pickle.load(open('scaler.pkl','rb'))
+X_scaled = sc.transform(l)
+
+res = m.predict(X_scaled)
 # print(m)
 # res = m.predict([[0.0035013571951045923,440.0,0.0029,0.00552454612,0.057438815952069316,0.00635000000000001,0.0024484]])
 mn =[]
